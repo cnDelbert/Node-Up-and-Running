4c2f81ff946af487f452c782afa4afab828a60ef
Delbert <code@delbert.me>
2015-02-09 21:46:53 +0800

    6.3.js

--- ch6/6.2.js
+++ ch6/6.2.js
@@ -9,17 +9,20 @@ var opts = {
 
 var callback = function(res){
 	console.log(res.statusCode)
-
+	res.on('data', function(chunk){
+		console.log(chunk.toString)
+		console.log('data');
+	})
 	// TODO: why not run here?
-	res.on('end', function(){
+	res.on('close', function(){
+		console.log('111111111')
 		if(res.statusCode == 201){
 			console.log('Database successfully created.')
 		} else {
 			console.log('Fail to create the database.')
 		}
-	})
+	});
 };
 
-var request = http.request(opts, callback);
-request.end();
+http.request(opts, callback).end();
 // if no end() method, it would be wrong for the next time.
---
+++ ch6/6.3.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+var opts = {
+	host: '127.0.0.1',
+	port: '5984',
+	path: '/dbname',
+	method: 'DELETE'
+}
+
+http.request(opts, function(response){
+	console.log(response.statusCode)
+	response.on('end', function(){
+		console.log('Deleted.')
+	})
+}).end();
