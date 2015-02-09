04f981e24c4689a5352fee321a7648910822ff0a
Delbert <code@delbert.me>
2015-02-09 19:57:03 +0800

    rewrite 6.1.js with http.requests().

--- ch6/6.1.js
+++ ch6/6.1.js
@@ -3,22 +3,25 @@ var http = require('http');
 // Start CouchDB first.
 var server = http.createServer(function(req, res){
 	// http.createClient is deprecated. Use `http.request` instead.
-	// var client = http.createClient(5984, '127.0.0.1');
-	// var request = client.request('GET', '/all_dbs');	
 
-	req = http.request(clientOpts, function(res){
-		console.log('Inside request');
-	});
-	req.end();
+	var clientOpts = {
+		hostname: 'localhost',
+		port: 5984,
+		path: '/_all_dbs',
+		method: 'GET'
+	};
 
-	req.on('response', function(response){
+	var callback = function(response){
 		var responseBody = '';
+		console.log('STATUS: ' + response.statusCode);
+		console.log('HEADERS: ' + JSON.stringify(response.headers));
+		response.setEncoding('utf8');
 
 		response.on('data', function(chunk){
 			responseBody += chunk;
 		});
 
-		response.on('end', function(res){
+		response.on('end', function(){
 			res.writeHead(200, {'Content-Type': 'text/plain'});
 			res.write(responseBody);
 			res.end();
@@ -27,34 +30,9 @@ var server = http.createServer(function(req, res){
 		response.on('error', function(e){
 			console.log(e);
 		});
-	});
+	};
 
-	req.on('error', function(e){
-		console.log('Problem: ' + e);
-	});
+	http.request(clientOpts, callback).end();
+}).listen(8080);
 
-});
-
-var clientOpts = {
-	hostname: 'localhost',
-	port: 5984,
-	path: '/all_dbs',
-	method: 'GET'
-}
-
-req = http.request(clientOpts, function(response){
-	console.log('Inside request');
-
-	var responseBody = '';
-
-	response.on('data', function(chunk){
-		responseBody += chunk;
-	});
-
-	response.on('error', function(e){
-		console.log(e);
-	});
-
-}).end();
-
-server.listen(8080);
+// rewrite with http.request().
