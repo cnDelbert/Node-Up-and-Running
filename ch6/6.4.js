1d44daa5be7527dafb88dc53e38424a4502c8655
Delbert <code@delbert.me>
2015-02-13 16:33:43 +0800

    6.4 done.

--- ch6/6.4.js
+++ ch6/6.4.js
@@ -12,13 +12,14 @@ deleteDb = function(res, dbpath){
 		path: dbpath,
 		method: 'GET'
 	};
-	var request = http.request(opts).end();
+	var request = http.request(opts);
+	request.end();
 
-	request.on('response', function(res){
-		console.log(res.statusCode);
-		res.on('data', function(){});
-		res.on('end', function(){
-			if (res.statusCode == 200){
+	request.on('response', function(response){
+		console.log(response.statusCode);
+		response.on('data', function(){});
+		response.on('end', function(){
+			if (response.statusCode == 200){
 				// database deleted.
 				showDbs(res, "Deleted database.");
 			}else{
@@ -27,7 +28,7 @@ deleteDb = function(res, dbpath){
 		});
 	});
 };
-// deleteDb();
+
 
 createDb = function(res, dbname){
 	var opts = {
@@ -36,12 +37,12 @@ createDb = function(res, dbname){
 		dbpath: '/',
 		method: 'PUT'
 	};
-	var request = http.request(opts).end();
-
-	request.on('response', function(res){
-		res.on('data', function(){});
-		res.on('end', function(){
-			if (res.statusCode == 201){
+	var request = http.request(opts);
+	request.end();
+	request.on('response', function(resp){
+		resp.on('data', function(){});
+		resp.on('end', function(){
+			if (resp.statusCode == 201){
 				showDbs(res, dbname + ' created.');
 			} else {
 				showDbs(res, "Could not create " + dbname);
@@ -49,3 +50,70 @@ createDb = function(res, dbname){
 		});
 	});
 };
+
+showDbs = function(res, message){
+	opts = {
+		host: dbHost,
+		port: dbPort,
+		dbpath: '/_all_dbs',
+		method: 'GET'
+	}
+	var request = http.request(opts);
+	request.end();
+
+	request.on('response', function(response){
+		var responseBody = '';
+		response.on('data', function (chunk) {
+			responseBody += chunk;
+		});
+
+		response.on('end', function(){
+			res.writeHead(200, {'Content-type': 'text/html'});
+			res.write('<form method="POST">');
+			res.write('New Database Name: <input type="text" name="dbname"/>');
+			res.write('<input type="submit" />');
+			res.write('</form>');
+			if (null != message){
+				res.write("<h1>" + message + "</h1>");
+			};
+
+			res.write("<h1>Active databases:</h1>");
+			res.write("<ul>");
+
+			var dblist = JSON.parse(responseBody);
+			for ( i = 0; i < dblist.length; i++){
+				var dbname = dblist[i];
+				res.write("<li><a href='/" + dbname +"'>" + dbname + "</a></li>");
+			};
+			res.write("</ul>");
+
+			res.end();
+		});
+	});
+};
+
+http.createServer(function(req, res){
+	if (req.method == 'POST'){
+		var body = '';
+		req.on('data', function(data){
+			body += data;
+		});
+		req.on('end', function(){
+			var POST = qs.parse(body);
+			var dbname = POST['dbname'];
+
+			if( null != dbname){
+				createDb(res, dbname);
+			} else {
+				showDbs(res, "Bad DB name, could not create database.");
+			};
+		})
+	} else {
+		var path = url.parse(req.url).pathname;
+		if(path != '/'){
+			deleteDb(res, path);
+		}else{
+			showDbs(res);
+		}
+	}
+}).listen(8080);
